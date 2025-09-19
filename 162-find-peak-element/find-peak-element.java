a795896fb4e30b60f16f215779e86135e19fe565
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-09-19 18:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (7.15%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left=0,right=nums.length-1,mid=0;
+
+        while(left<right){
+            mid = (left+right) >>> 1;
+
+            if(nums[mid]<nums[mid+1]) left = mid+1;
+            else right = mid;
+        }
+        return left;
+    }
+}
