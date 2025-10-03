a50a798ec305fb42cec1a340dba08dcb7d74c006
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-03 22:23:16 +0530

    Time: 2 ms (31.72%) | Memory: 45.2 MB (54.11%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -25,3 +25,45 @@ class Solution {
         return true;
     }
 }
+
+
+// class Solution {
+
+//     public boolean dfs(int src, int par, int[] color, int[][] graph, int colorToFill){
+
+//         color[src] = colorToFill;
+
+//         for(int v : graph[src]){
+
+//             if(color[v] == -1){
+//                boolean ans = dfs(v, src, color, graph, 1 - color[src]);
+//                if(ans == false){
+//                 return false;
+//                }
+//             }else if(color[v] == colorToFill){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public boolean isBipartite(int[][] graph) {
+        
+//         int n = graph.length;
+//         int[] color = new int[n];
+//         Arrays.fill(color, -1);
+
+//         for(int s = 0 ; s < n ; s++){
+
+//             if(color[s] == -1){
+
+//                 boolean ans = dfs(s, -1, color, graph, 1);
+
+//                 if(ans == false){
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return true;
+//     }
+// }
