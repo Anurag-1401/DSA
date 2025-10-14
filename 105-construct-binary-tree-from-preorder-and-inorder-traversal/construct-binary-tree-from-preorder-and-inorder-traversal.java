d15e275b86539b6db030c0ba3e9ce72b2ad935fc
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-14 16:34:42 +0530

    Time: 1 ms (96.34%) | Memory: 43.8 MB (98.30%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -41,3 +41,26 @@ class Solution {
         return tree;
     }
 }
+
+// class Solution {
+//     int preIdx = 0;
+//     int inIdx = 0;
+
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         return dfs(preorder, inorder, Integer.MAX_VALUE);
+//     }
+
+//     private TreeNode dfs(int[] preorder, int[] inorder, int limit) {
+//         if(preIdx == preorder.length) return null;
+//         if(inorder[inIdx] == limit) {
+//             inIdx++;
+//             return null;
+//         }
+
+//         TreeNode newNode = new TreeNode(preorder[preIdx++]);
+//         newNode.left = dfs(preorder, inorder, newNode.val);
+//         newNode.right = dfs(preorder, inorder, limit);
+
+//         return newNode;
+//     }
+// }
