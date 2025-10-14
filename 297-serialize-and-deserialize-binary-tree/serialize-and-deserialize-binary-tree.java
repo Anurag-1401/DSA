831c72bbf8ca5506aff179dd0c46adabe648ff41
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-14 09:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (98.80%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -7,28 +7,64 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Codec {
+// public class Codec {
 
+//     // Encodes a tree to a single string.
+//     public String serialize(TreeNode root) {
+//         if(root == null) return "null";
+//         return root.val + "," + serialize(root.left) + "," + serialize(root.right); 
+//     }
+
+//     // Decodes your encoded data to tree.
+//     public TreeNode deserialize(String data) {
+//         Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(",")));
+//         return build(nodes); 
+//     }
+//     private TreeNode build(Queue<String> nodes){
+//         String val = nodes.poll();
+//         if(val.equals("null")) return null;
+
+//         TreeNode tree = new TreeNode(Integer.parseInt(val));
+//         tree.left = build(nodes);
+//         tree.right = build(nodes);
+        
+//         return tree;
+//     }
+// }
+
+
+public class Codec {
+    class IdNode {
+        int val;
+        int left_id;
+        int right_id;
+    }
+    static TreeNode temp = null;
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if(root == null) return "null";
-        return root.val + "," + serialize(root.left) + "," + serialize(root.right); 
+        temp = root;
+        // ArrayList<IdNode> nodeList = new ArrayList();
+        return new String();
+    }
+    
+    private int toNodeList(TreeNode node, ArrayList<IdNode> list) {
+        if (node == null) {
+            return 0;
+        }
+        
+        IdNode idNode = new IdNode();
+        int node_id = list.size();
+        list.add(idNode);
+        idNode.val = node.val;
+        idNode.left_id = toNodeList(node.left, list);
+        idNode.right_id = toNodeList(node.right, list);
+            
+        return node_id;
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(",")));
-        return build(nodes); 
-    }
-    private TreeNode build(Queue<String> nodes){
-        String val = nodes.poll();
-        if(val.equals("null")) return null;
-
-        TreeNode tree = new TreeNode(Integer.parseInt(val));
-        tree.left = build(nodes);
-        tree.right = build(nodes);
-        
-        return tree;
+        return temp;
     }
 }
 
