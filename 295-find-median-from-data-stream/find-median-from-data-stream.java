7030658e0e09d48f1e0a0a5c6c746c770cfd361c
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-16 16:43:07 +0530

    Time: 2004 ms (5.01%) | Memory: 64.9 MB (7.27%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,25 +1,41 @@
-import java.util.*;
-
 class MedianFinder {
-    private PriorityQueue<Integer> left; 
-    private PriorityQueue<Integer> right; 
+    private int[] nums;
+    private int size;
 
     public MedianFinder() {
-        left = new PriorityQueue<>(Collections.reverseOrder()); 
-        right = new PriorityQueue<>();                         
+        nums = new int[10]; // start small; will auto-expand
+        size = 0;
     }
-    
+
     public void addNum(int num) {
-        left.offer(num);
-        right.offer(left.poll());        
-        if (left.size() < right.size()) {
-            left.offer(right.poll());    
+        if (size == nums.length) resize(); // \U0001f527 expand when full
+
+        int i = size - 1;
+        // insert in sorted order
+        while (i >= 0 && nums[i] > num) {
+            nums[i + 1] = nums[i];
+            i--;
         }
+        nums[i + 1] = num;
+        size++;
     }
-    
+
+    private void resize() {
+        int[] newArr = new int[nums.length * 2]; // double capacity
+        for (int i = 0; i < nums.length; i++) {
+            newArr[i] = nums[i];
+        }
+        nums = newArr;
+    }
+
     public double findMedian() {
-        if (left.size() > right.size()) return left.peek();
-        return (left.peek() + right.peek()) / 2.0;
+        if (size == 0) return 0;
+
+        if (size % 2 == 1) {
+            return nums[size / 2];
+        } else {
+            return (nums[size / 2 - 1] + nums[size / 2]) / 2.0;
+        }
     }
 }
 
