6a7e09656707005f4305f569881b115b9c0991a0
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-30 10:51:21 +0530

    Time: 3 ms (99.74%) | Memory: 44.9 MB (90.31%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,27 +1,35 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int count=0,curr=0;
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] ind = new int[numCourses];
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
-
-        for(int[] p : prerequisites){
-            graph.get(p[1]).add(p[0]);
-            ind[p[0]]++;
-        }
-
-        for(int i=0;i<numCourses;i++) if(ind[i] == 0) q.offer(i);
-
-        while(!q.isEmpty()){
-            curr = q.poll();
-            count++;
-            for(int neigh : graph.get(curr)){
-                ind[neigh]--;
-                if(ind[neigh] == 0) q.offer(neigh);
+    boolean pos=true;
+    
+    public void traverse(List<Integer>[] adj,int start,int[] visited){
+        if(visited[start]==0){
+            visited[start]=1;
+            for(int i=0;i<adj[start].size();i++)
+            {
+                traverse(adj,adj[start].get(i),visited);
             }
+            visited[start]=2;
+        } else {
+            if(visited[start]==1){
+                pos=false;
+            }
+            return;
         }
-        return count == numCourses;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adj=new ArrayList[numCourses];
+
+        int[] visited=new int[numCourses];
+
+        for(int i=0;i<numCourses;i++) adj[i]=new ArrayList<>();
+
+        for(int i=0;i<prerequisites.length;i++) adj[prerequisites[i][1]].add(prerequisites[i][0]);
+        for(int i=0;i<visited.length;i++){
+            pos=true;
+            traverse(adj,i,visited);
+                if(pos==false) return false;
+        }
+        
+        return pos;
     }
 }
