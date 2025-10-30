61def986ec40c98caca733d164cf9afb3bbeb613
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-30 10:38:53 +0530

    Time: 23 ms (99.84%) | Memory: 42.7 MB (88.74%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -16,25 +16,48 @@ class Node {
         neighbors = _neighbors;
     }
 }
-*/
+// */
+// class Solution {
+//     Map<Node, Node> originalNodeToClonedNodeMap = new HashMap<>();
+
+//     public Node cloneGraph(Node node) {
+//         if(node == null) return null;
+
+//         Node clonedNode = new Node(node.val);
+//         originalNodeToClonedNodeMap.put(node, clonedNode);
+
+//         for(Node neighbor: node.neighbors) {
+//             if(originalNodeToClonedNodeMap.containsKey(neighbor)) {
+//                 clonedNode.neighbors.add(originalNodeToClonedNodeMap.get(neighbor));
+//             }
+//             else {
+//                 clonedNode.neighbors.add(cloneGraph(neighbor));
+//             }
+//         }
+
+//         return clonedNode;
+//     }
+// }
+
 class Solution {
-    Map<Node, Node> originalNodeToClonedNodeMap = new HashMap<>();
-
     public Node cloneGraph(Node node) {
-        if(node == null) return null;
+        return cloneNode(node, new HashMap<>());
+    }
 
-        Node clonedNode = new Node(node.val);
-        originalNodeToClonedNodeMap.put(node, clonedNode);
+    private Node cloneNode(Node original, Map<Node, Node> visited) {
+        if (original == null) return null;
 
-        for(Node neighbor: node.neighbors) {
-            if(originalNodeToClonedNodeMap.containsKey(neighbor)) {
-                clonedNode.neighbors.add(originalNodeToClonedNodeMap.get(neighbor));
-            }
-            else {
-                clonedNode.neighbors.add(cloneGraph(neighbor));
-            }
+        if (visited.containsKey(original)) {
+            return visited.get(original);
         }
 
-        return clonedNode;
+        Node copy = new Node(original.val);
+        visited.put(original, copy);
+
+        for (Node neighbor : original.neighbors) {
+            copy.neighbors.add(cloneNode(neighbor, visited));
+        }
+
+        return copy; 
     }
 }
