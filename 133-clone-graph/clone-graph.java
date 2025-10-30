cdf01c6094465a597ffe01f9c80b5306ad2ef1c8
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-30 10:39:39 +0530

    Time: 25 ms (64.71%) | Memory: 43.2 MB (13.34%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -16,48 +16,49 @@ class Node {
         neighbors = _neighbors;
     }
 }
-// */
-// class Solution {
-//     Map<Node, Node> originalNodeToClonedNodeMap = new HashMap<>();
-
-//     public Node cloneGraph(Node node) {
-//         if(node == null) return null;
-
-//         Node clonedNode = new Node(node.val);
-//         originalNodeToClonedNodeMap.put(node, clonedNode);
-
-//         for(Node neighbor: node.neighbors) {
-//             if(originalNodeToClonedNodeMap.containsKey(neighbor)) {
-//                 clonedNode.neighbors.add(originalNodeToClonedNodeMap.get(neighbor));
-//             }
-//             else {
-//                 clonedNode.neighbors.add(cloneGraph(neighbor));
-//             }
-//         }
-
-//         return clonedNode;
-//     }
-// }
+*/
 
 class Solution {
+    Map<Node, Node> originalNodeToClonedNodeMap = new HashMap<>();
+
     public Node cloneGraph(Node node) {
-        return cloneNode(node, new HashMap<>());
-    }
+        if(node == null) return null;
 
-    private Node cloneNode(Node original, Map<Node, Node> visited) {
-        if (original == null) return null;
+        Node clonedNode = new Node(node.val);
+        originalNodeToClonedNodeMap.put(node, clonedNode);
 
-        if (visited.containsKey(original)) {
-            return visited.get(original);
+        for(Node neighbor: node.neighbors) {
+            if(originalNodeToClonedNodeMap.containsKey(neighbor)) {
+                clonedNode.neighbors.add(originalNodeToClonedNodeMap.get(neighbor));
+            }
+            else {
+                clonedNode.neighbors.add(cloneGraph(neighbor));
+            }
         }
 
-        Node copy = new Node(original.val);
-        visited.put(original, copy);
-
-        for (Node neighbor : original.neighbors) {
-            copy.neighbors.add(cloneNode(neighbor, visited));
-        }
-
-        return copy; 
+        return clonedNode;
     }
 }
+
+// class Solution {
+//     public Node cloneGraph(Node node) {
+//         return cloneNode(node, new HashMap<>());
+//     }
+
+//     private Node cloneNode(Node original, Map<Node, Node> visited) {
+//         if (original == null) return null;
+
+//         if (visited.containsKey(original)) {
+//             return visited.get(original);
+//         }
+
+//         Node copy = new Node(original.val);
+//         visited.put(original, copy);
+
+//         for (Node neighbor : original.neighbors) {
+//             copy.neighbors.add(cloneNode(neighbor, visited));
+//         }
+
+//         return copy; 
+//     }
+// }
