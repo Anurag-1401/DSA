c5a7e303fe8310cc9c9307c57e64454da3aa309a
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-10-30 10:05:30 +0530

    Update find-median-from-data-stream.java

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -50,6 +50,50 @@ class MedianFinder {
     }
 }
 
+
+
+// class MedianFinder {
+//     private int[] nums;
+//     private int size;
+
+//     public MedianFinder() {
+//         nums = new int[10]; // start small; will auto-expand
+//         size = 0;
+//     }
+
+//     public void addNum(int num) {
+//         if (size == nums.length) resize(); // 🔧 expand when full
+
+//         int i = size - 1;
+//         // insert in sorted order
+//         while (i >= 0 && nums[i] > num) {
+//             nums[i + 1] = nums[i];
+//             i--;
+//         }
+//         nums[i + 1] = num;
+//         size++;
+//     }
+
+//     private void resize() {
+//         int[] newArr = new int[nums.length * 2]; // double capacity
+//         for (int i = 0; i < nums.length; i++) {
+//             newArr[i] = nums[i];
+//         }
+//         nums = newArr;
+//     }
+
+//     public double findMedian() {
+//         if (size == 0) return 0;
+
+//         if (size % 2 == 1) {
+//             return nums[size / 2];
+//         } else {
+//             return (nums[size / 2 - 1] + nums[size / 2]) / 2.0;
+//         }
+//     }
+// }
+
+
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
