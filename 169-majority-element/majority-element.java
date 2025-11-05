8280937dac7386a2a3cad6547b73ada11379cb44
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-05 12:52:30 +0530

    Time: 1 ms (99.77%) | Memory: 55.5 MB (6.87%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int major = 0,freq = 0;
+
+        for(int num : nums){
+            if(freq == 0) major = num;
+            if(num == major) freq++;
+            else freq--;
+        }
+        return major;
+    }
+}
