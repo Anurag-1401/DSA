ef4be60ae63ca324e12f1828818031a2b8e9d1d0
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-06 19:54:29 +0530

    Time: 1 ms (99.85%) | Memory: 48.9 MB (5.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,14 +1,33 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> merged = new LinkedList<>();
-
-        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
-
-        for(int[] interval : intervals){
-            if(merged.isEmpty() || merged.getLast()[1] < interval[0]) merged.add(interval);
-            else merged.getLast()[1] = Math.max(merged.getLast()[1],interval[1]);
-        }
-
-        return merged.toArray(new int[merged.size()][]);
-    }
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < intervals.length; i++) {
+			min = Math.min(min, intervals[i][0]);
+			max = Math.max(max, intervals[i][0]);
+		}
+		
+		int[] range = new int[max - min + 1];
+		for (int i = 0; i < intervals.length; i++) {
+			range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]); 
+		}
+		
+		int start = 0, end = 0;
+		LinkedList<int[]> result = new LinkedList<>();
+		for (int i = 0; i < range.length; i++) {
+			if (range[i] == 0) {
+				continue;
+			}
+			if (i <= end) {
+				end = Math.max(range[i], end);
+			} else {
+				result.add(new int[] {start + min, end + min});
+				start = i;
+				end = range[i];
+			}
+		}
+		result.add(new int[] {start + min, end + min});
+		return result.toArray(new int[result.size()][]);
+	}
 }
