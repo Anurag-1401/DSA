4b4e77f14a77ab64ef0f453d057a2013dba7e426
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-19 15:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (27.89%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
+     List<String> ans = new ArrayList<>();
+    dfs(n, n, new StringBuilder(), ans);
+    return ans;
+  }
 
-        backtrack("", 0, 0, n, result);
-
-        return result;
+  private void dfs(int l, int r, StringBuilder sb, List<String> ans) {
+    if (l == 0 && r == 0) {
+      ans.add(sb.toString());
+      return;
     }
 
-    private void backtrack(String current, int open, int close, int n, List<String> result) {
-        if (current.length() == 2 * n) {
-            result.add(current);
-            return;
-        }
-
-        if (open < n) {
-            backtrack(current + "(", open + 1, close, n, result);
-        }
-        if (close < open) {
-            backtrack(current + ")", open, close + 1, n, result);
-        }
+    if (l > 0) {
+      sb.append("(");
+      dfs(l - 1, r, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
+    }
+    if (l < r) {
+      sb.append(")");
+      dfs(l, r - 1, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
+    }   
     }
 }
