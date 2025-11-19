a7b44f001a28d138b03786d25426be79550122bd
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-19 15:52:37 +0530

    Time: 1 ms (99.89%) | Memory: 45.8 MB (13.19%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(candidates);
-        backtrack(res,new ArrayList<>(),candidates,target,0);
-        return res;
-    }
-    private void backtrack(List<List<Integer>> result,List<Integer> current,int[] cand,int t,int start){
-        if (t == 0){
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        for(int i = start;i<cand.length;i++){
-            if(cand[i]>t) break;
-            current.add(cand[i]);
-            backtrack(result,current,cand,t - cand[i],i);
-            current.remove(current.size()-1);
-        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        recursion(0 , ans , new ArrayList<>() , target , candidates);
+
+        return ans;
         
+    }
+
+    public void recursion(int index , List<List<Integer>> ans , List<Integer> list, int target , int[] arr){
+
+        if(target == 0){
+
+            ans.add(new ArrayList<>(list));
+        }
+
+        for(int i = index ; i < arr.length ; i++){
+
+            if(arr[i] > target) continue;
+
+            list.add(arr[i]);
+            recursion(i , ans , list , target-arr[i] , arr);
+            list.remove(list.size()-1);
+        }
     }
 }
