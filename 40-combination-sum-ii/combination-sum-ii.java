4eac7a2be4b5b1e8c521d021e67bcaa542fe099b
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-19 15:54:14 +0530

    Time: 2 ms (99.58%) | Memory: 44.9 MB (25.89%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);  // Required to skip duplicates
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(0, candidates, target, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+
+        int[] freq = new int[51];
+        for(int num: candidates) {
+            freq[num]++;
+        }
+
+        backtrack(freq, target, res, new ArrayList<>(), 0);
+        return res;
     }
 
-    private void backtrack(int index, int[] arr, int target,
-                           List<Integer> curr, List<List<Integer>> result) {
-
-        if (target == 0) {                // Found a valid combination
-            result.add(new ArrayList<>(curr));
+    private void backtrack(int[] freq, int target, List<List<Integer>> res, List<Integer> currComb, int idx) {
+        if (target < 0)
+            return;
+        
+        if (target == 0) {
+            res.add(new ArrayList<>(currComb));
             return;
         }
 
-        if (index == arr.length || target < 0) return;
-
-        for (int i = index; i < arr.length; i++) {
-
-            // Skip duplicates
-            if (i > index && arr[i] == arr[i - 1]) continue;
-
-            if (arr[i] > target) break; // No point in continuing
-
-            curr.add(arr[i]);  // Choose element
-
-            backtrack(i + 1, arr, target - arr[i], curr, result);
-
-            curr.remove(curr.size() - 1); // Backtrack
+        for (int i=idx; i<freq.length && i <= target; i++) {
+            if (freq[i] > 0) {
+                freq[i]--;
+                currComb.add(i);
+                backtrack(freq, target-i, res, currComb, i);
+                currComb.removeLast();
+                freq[i]++;
+            }
         }
     }
 }
