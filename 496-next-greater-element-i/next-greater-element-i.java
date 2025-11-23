21c942b4551d665cf94977f6fa09ff43f86a3e60
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-23 10:32:46 +0530

    Time: 1 ms (97.31%) | Memory: 45.4 MB (6.15%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,34 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // HashMap<Integer,Integer> greater = new HashMap<>();
-        Deque<Integer> stack = new ArrayDeque<>();
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] arrIdx = new int[10001];
 
-        // for(int num: nums2){
-        //     while(!stack.isEmpty() && num > stack.peek()){
-        //         greater.put(stack.peek(),num);
-        //     }
-        //     stack.push(num);
-        // }
+        for(int i = 0;i <n2;i++){
+            arrIdx[nums2[i]] = i;
+        }
 
-        // while(!stack.isEmpty()){
-        //     greater.put(stack.peek(),-1);
-        // }
+        for(int i = 0;i < n1;i++){
+            nums1[i] = findNextgreater(nums2,nums1[i],arrIdx,n2);
+        }
 
-        int[] next = new int[10001];
-        Arrays.fill(next,-1);
+        return nums1;
+    }
 
-        for(int num:nums2){
-            while(!stack.isEmpty() && stack.peek() < num){
-                next[stack.pop()] = num;
+    public int findNextgreater(int[] arr,int greater,int[] arrIdx,int n){
+
+        for(int i = arrIdx[greater] + 1;i < n;i++){
+            if(arr[i] > greater){
+                return arr[i];
             }
-            stack.push(num);
         }
 
-        int[] ans = new int[nums1.length];
-        for(int i = 0;i < nums1.length;i++){
-            // ans[i] = greater.get(nums1[i]);
-            ans[i] = next[nums1[i]];
-        }
-        return ans;
+        return -1;
     }
 }
