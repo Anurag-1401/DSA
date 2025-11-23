db3c5ed0a36f7b9db2b162491783272b6b94e746
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-23 10:16:33 +0530

    Time: 3 ms (68.88%) | Memory: 43.4 MB (14.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -13,3 +13,44 @@ class Solution {
         return para.isEmpty();
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         char[] arr=new char[s.length()];
+//         int top=-1;
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch=='(' || ch=='{' || ch=='['){
+//                 top++;
+//                 arr[top]=ch;
+//             }
+//             else{
+//                 if(top==-1){
+//                     return false;
+//                 }
+//                 char last=arr[top];
+//                 top--;
+//                 if(ch==')' && last !='('){
+//                     return false;
+//                 }
+//                 if(ch=='}' && last !='{'){
+//                     return false;
+//                 }
+//                 if(ch==']' && last !='['){
+//                     return false;
+//                 }
+
+//             }
+//         }
+//         if(top==-1){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+        
+//     }
+// }
