e15a947f4ce715e80da9b6c2de7cce671ccf8246
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-24 21:48:40 +0530

    Time: 5 ms (99.58%) | Memory: 46.4 MB (19.25%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,36 +1,36 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-
-        long sumMax = 0;
-        long sumMin = 0;
-
-        Stack<Integer> st = new Stack<>();
-
-        // ------- Contribution as Maximum --------
+        long result = 0;
+        
+        // Use arrays as stacks (stores indices)
+        int[] minStack = new int[n];
+        int[] maxStack = new int[n];
+        int minTop = -1, maxTop = -1;
+        
         for (int i = 0; i <= n; i++) {
-            while (!st.isEmpty() && (i == n || nums[st.peek()] < nums[i])) {
-                int mid = st.pop();
-                int left = st.isEmpty() ? mid + 1 : mid - st.peek();
-                int right = i - mid;
-                sumMax += (long) nums[mid] * left * right;
+            // Process min stack (sum of minimums)
+            while (minTop >= 0 && (i == n || nums[minStack[minTop]] >= nums[i])) {
+                int j = minStack[minTop--];
+                long left = minTop < 0 ? j + 1 : j - minStack[minTop];
+                long right = i - j;
+                result -= (long) nums[j] * left * right;
             }
-            st.push(i);
-        }
-
-        st.clear();
-
-        // ------- Contribution as Minimum --------
-        for (int i = 0; i <= n; i++) {
-            while (!st.isEmpty() && (i == n || nums[st.peek()] > nums[i])) {
-                int mid = st.pop();
-                int left = st.isEmpty() ? mid + 1 : mid - st.peek();
-                int right = i - mid;
-                sumMin += (long) nums[mid] * left * right;
+            
+            // Process max stack (sum of maximums)  
+            while (maxTop >= 0 && (i == n || nums[maxStack[maxTop]] <= nums[i])) {
+                int j = maxStack[maxTop--];
+                long left = maxTop < 0 ? j + 1 : j - maxStack[maxTop];
+                long right = i - j;
+                result += (long) nums[j] * left * right;
             }
-            st.push(i);
+            
+            if (i < n) {
+                minStack[++minTop] = i;
+                maxStack[++maxTop] = i;
+            }
         }
-
-        return sumMax - sumMin;
+        
+        return result;
     }
 }
