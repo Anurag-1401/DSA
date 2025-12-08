7145e78912ffe6b9c9960ef4d4fe5bf54cf1a6c2
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-08 14:54:49 +0530

    Time: 1 ms (99.73%) | Memory: 47.9 MB (16.85%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int a =0;
+
+        for(int num : nums){
+            if(num !=0) nums[a++] = num;
+        }
+
+        while(a<nums.length) nums[a++] = 0;
+    }
+}
