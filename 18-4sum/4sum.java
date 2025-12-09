ddb306f7981a8bdeb4e55eda73d206f6fcb43ae8
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-09 17:33:48 +0530

    Time: 5 ms (97.67%) | Memory: 46 MB (13.07%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,31 +1,32 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int k) {
-        int left=0,right=0;
-        long target=0;
-
+    public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            long s1 = (long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3];
+            if (s1 > target) break;
+            s1 = (long) nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1];
+            if (s1 < target) continue;
 
-        for(int i=0;i<nums.length-3;i++){
-            if(i>0 && nums[i-1]== nums[i]) continue;
+            long t1 = (long) target - nums[i];
+            for (int j = i + 1; j < n - 2; j ++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                long s2 = (long) nums[j] + nums[j + 1] + nums[j + 2];
+                if (s2 > t1) break;
+                s2 = (long) nums[j] + nums[n - 2] + nums[n - 1];
+                if (s2 < t1) continue;
 
-            for(int j=i+1;j<nums.length-2;j++){
-                if(j>i+1 && nums[j-1] == nums[j]) continue;
-
-                left = j+1;
-                right = nums.length-1;
-
-                while(left<right){
-                    target = (long) nums[i]+nums[j]+nums[left]+nums[right];
-                    if(target == k) {
-                        res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        left++;
-                        right--;
-
-                        while(left<right && nums[left-1] == nums[left]) left++;
-                        while(left<right && nums[right+1] == nums[right]) right--;
-                    } else if(target < k) left++;
-                    else right--;
+                long t2 = (long) target - nums[i] - nums[j];
+                for (int k = j + 1, l = n - 1; k < l; ) {
+                    long sum = (long) nums[k] + nums[l];
+                    if (sum == t2) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[k++], nums[l--]));
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < t2) k++;
+                    else l--;
                 }
             }
         }
