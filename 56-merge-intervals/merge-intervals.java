6e9602a8923d2b162703d023f264a6b0c98edfc9
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-10 15:46:04 +0530

    Time: 2 ms (99.10%) | Memory: 49.1 MB (20.96%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,13 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        int max = -1,min = Integer.MAX_VALUE;
+
         LinkedList<int[]> ans = new LinkedList<>();
 
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-
-        for(int[] interval:intervals){
-            if(ans.isEmpty() || ans.getLast()[1]<interval[0]) ans.add(interval);
-            else ans.getLast()[1] = Math.max(ans.getLast()[1],interval[1]);
+        for(int i=0;i<intervals.length;i++){
+            if(intervals[i][0]>max) max = intervals[i][0];
+            if(intervals[i][0]<min) min = intervals[i][0];
         }
+
+        int[] range = new int[max-min+1];
+        for(int i=0;i<intervals.length;i++){
+            range[intervals[i][0]-min] = Math.max(intervals[i][1]-min,range[intervals[i][0]-min]);
+        }
+
+        int start=0,end=0;
+        for(int i=0;i<range.length;i++){
+            if(range[i] == 0) continue;
+            if(i<=end) end = Math.max(end,range[i]);
+            else {
+                ans.add(new int[]{start+min,end+min});
+                start = i;
+                end = range[i];
+            }
+        }
+        ans.add(new int[]{start+min,end+min});
+
         return ans.toArray(new int[ans.size()][]);
     }
 }
