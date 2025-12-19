92937afcf55cb0df45079195a0a4ea4bded139a5
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-19 16:30:36 +0530

    Time: 3 ms (99.69%) | Memory: 63.5 MB (10.23%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return  head;
+
+        int min = Integer.MAX_VALUE,max = Integer.MIN_VALUE;
+        ListNode temp = head;
+
+        while(temp!=null){
+            if(temp.val>max) max = temp.val;
+            if(temp.val<min) min = temp.val;
+            temp = temp.next;
+        }
+
+        int[] freq = new int[max-min+1];
+        temp=head;
+        while(temp!=null){
+            freq[temp.val-min]++;
+            temp = temp.next;
+        }
+
+        temp = head;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]-- >0) {
+                temp.val=i+min;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
