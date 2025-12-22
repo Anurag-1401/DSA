bd1d00d83bd7cf4ff18f8107c0585204783ce36d
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-22 13:04:35 +0530

    Time: 1 ms (99.88%) | Memory: 43.7 MB (45.60%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,25 +1,61 @@
 class Solution {
-    public int myAtoi(String s) {
-        int i = 0, n = s.length();
+        int i = 0;
         long num = 0;
         int sign = 1;
+        boolean start = false;
 
-        while (i < n && s.charAt(i) == ' ') i++;
+    public int myAtoi(String s) {
+        // int n = s.length();
 
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-') sign = -1;
+        // while (i < n && s.charAt(i) == ' ') i++;
+
+        // if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+        //     if (s.charAt(i) == '-') sign = -1;
+        //     i++;
+        // }
+
+        // while (i < n && Character.isDigit(s.charAt(i))) {
+        //     num = num * 10 + (s.charAt(i) - '0');
+
+        //     if (sign == 1 && num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        //     if (sign == -1 && -num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        //     i++;
+        // }
+
+        // return (int)(sign * num);
+
+
+
+
+
+        if(i>=s.length()) return (int)(sign*num);
+
+        char ch = s.charAt(i);
+
+        if(!start && ch == ' '){
             i++;
+            return myAtoi(s);
         }
 
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            num = num * 10 + (s.charAt(i) - '0');
-
-            if (sign == 1 && num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign == -1 && -num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
+        if(!start && (ch == '+' || ch == '-')){
+            start = true;
+            sign = ch == '+' ? 1:-1;
             i++;
+            return myAtoi(s);
         }
 
-        return (int)(sign * num);
+        if(Character.isDigit(ch)){
+            start = true;
+
+            num = num*10 + (ch-'0');
+
+            if(sign == 1 && num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if(sign == -1 && -num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+
+            return myAtoi(s);
+        }
+        return (int) (sign*num);
     }
 }
