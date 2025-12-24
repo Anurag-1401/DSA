b3d42e3f471ef8be6b131d20a0dc6ee9df48a6f5
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-24 19:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (26.75%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,70 @@
+
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int rows = board.length,cols = board[0].length;
+        int n = board.length;
+        int m = board[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        
+        char[] wordChar = word.toCharArray();
 
-        for(int r=0;r<rows;r++){
-            for(int c=0;c<cols;c++){
-                if(visit(0,r,c,word,board)) return true;
+        if (wordChar.length > n * m)
+            return false;
+        
+        int counts[] = new int[256];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                counts[board[i][j]]++;
+            }
+        }
+        
+        int len = wordChar.length;
+        for(int i=0; i<len/2; i++) {
+            if(counts[wordChar[i]] > counts[wordChar[len - 1 - i]]) {
+                for(int j=0; j<len/2; j++) {
+                    char temp = wordChar[j];
+                    wordChar[j] = wordChar[len - 1 - j];
+                    wordChar[len - 1 - j] = temp;
+                }
+                break;
+            }
+        }
+        
+        for (char c : wordChar) {
+            if (--counts[c] < 0)
+                return false;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visit(board, wordChar, 0, i, j, n, m, visited))
+                    return true;
             }
         }
         return false;
     }
 
-    private boolean visit(int i,int r,int c,String word,char[][] b){
-        if(i==word.length()) return true;
-
-        if(c<0 || r<0 || c>=b[0].length || r>=b.length || word.charAt(i) != b[r][c]) return false;
-
-        char ch = b[r][c];
-
-        b[r][c] ='#';
-
-        boolean found = 
-            visit(i+1,r+1,c,word,b) ||
-            visit(i+1,r-1,c,word,b) ||
-            visit(i+1,r,c+1,word,b) ||
-            visit(i+1,r,c-1,word,b);
-
-        b[r][c] = ch;
-
+    private boolean visit(char[][] board, char[] word, int start, int x, int y,
+            int n, int m, boolean[][] visited) {
+        if (start == word.length)
+            return true;
+        
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y])
+            return false;
+        
+        if (word[start] != board[x][y])
+            return false;
+        
+        visited[x][y] = true;
+        
+        boolean found = visit(board, word, start + 1, x + 1, y, n, m, visited)
+                || visit(board, word, start + 1, x - 1, y, n, m, visited)
+                || visit(board, word, start + 1, x, y + 1, n, m, visited)
+                || visit(board, word, start + 1, x, y - 1, n, m, visited);
+        
+        visited[x][y] = false;
+        
         return found;
     }
 }
