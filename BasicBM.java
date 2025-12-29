c501097bb3d9533da51c93f7a78f7c8f9767b54b
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-29 17:27:46 +0530

    Add methods for bit manipulation in BasicBM.java

---
+++ BasicBM.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countSetBits(int n) {
+        int c=0;
+        while(n!=0){
+            n&=(n-1);
+            c++;
+        }
+        return c;
+    }
+}
+
+
+
+
+class Solution {
+    public boolean checkIthBit(int n, int i) {
+        return (n & (1<<i)) != 0;
+    }
+}
+
+
+
+
+
+class Solution {
+    public boolean isOdd(int n) {
+        return (n&1)==1;
+    }
+}
