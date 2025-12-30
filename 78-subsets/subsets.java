4d71bf445de356d34a75a90abf15338ccc523e1e
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-12-30 18:27:00 +0530

    Fix formatting and comment out Solution class
    Added missing newline at the end of the file and commented out the Solution class.

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -18,3 +18,23 @@ class Solution {
         curr.remove(curr.size()-1);
     }
 }
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         int n = nums.length;
+//         int total  = 1<<n;
+
+//         List<List<Integer>> res = new ArrayList<>();
+//         for(int i=0;i<total;i++){
+//             List<Integer> subset = new ArrayList<>();
+//             for(int j=0;j<n;j++){
+//                 if((i&(1<<j))!=0) subset.add(nums[j]);
+//             }
+//             res.add(subset);
+//         }
+//         return res;
+//     }
+// }
