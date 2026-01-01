96f7f16df713236edc3c21d4f8b0183022b5c866
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-01-01 18:43:26 +0530

    Time: 3 ms (87.57%) | Memory: 43.2 MB (56.12%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        char[] arr = s.toCharArray();
+
+        for(char c:arr){
+            if(c=='(' || c=='[' || c=='{') st.push(c);
+            else{
+                if(st.isEmpty()) return false;
+                char top = st.pop();
+                if(top == '(' && c != ')' || 
+                top == '[' && c != ']' || 
+                top == '{' && c != '}') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
