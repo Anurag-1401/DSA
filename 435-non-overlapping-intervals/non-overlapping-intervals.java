23fa60a2ff0208e5cbefaaf2aa3f71f853d43c72
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-01-15 12:18:00 +0530

    Time: 4 ms (99.62%) | Memory: 113.8 MB (99.87%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,18 +1,45 @@
+// class Solution {
+//     public int eraseOverlapIntervals(int[][] intervals) {
+//         int n = intervals.length;
+
+//         Arrays.sort(intervals,(a,b) -> a[1]-b[1]);
+
+//         int count =1,lastEnd = intervals[0][1];
+
+//         for(int i=1;i<n;i++){
+//             if(intervals[i][0] >= lastEnd){
+//                 count++;
+//                 lastEnd = intervals[i][1];
+//             }
+//         }
+
+//         return n - count;
+//     }
+// }
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int n = intervals.length;
-
-        Arrays.sort(intervals,(a,b) -> a[1]-b[1]);
-
-        int count =1,lastEnd = intervals[0][1];
-
-        for(int i=1;i<n;i++){
-            if(intervals[i][0] >= lastEnd){
+        int max = intervals[0][1];
+        int min = max;
+        for (int i = 1; i < intervals.length; i++) {
+            max = Math.max(max, intervals[i][1]);
+            min = Math.min(min, intervals[i][1]);
+        }
+        int shift = 1 - min;
+        int[] rightEnds = new int[max - min + 2];
+        for (int[] interval : intervals) {
+            int left = interval[0] + shift;
+            int right = interval[1] + shift;
+            if (rightEnds[right] < left) rightEnds[right] = left;
+        }
+        int count = 0;
+        int start = 0;
+        for (int i = 1; i < rightEnds.length; i++) {
+            if (start <= rightEnds[i]) {
                 count++;
-                lastEnd = intervals[i][1];
+                start = i;
             }
         }
-
-        return n - count;
+        return intervals.length - count;
     }
 }
