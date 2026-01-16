13a2bbab058564d0d2ce4d02a409d598e14377f8
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-01-16 19:43:47 +0530

    Create bottomView.java

---
+++ bottomView.java
@@ -0,0 +1,43 @@
+
+class Solution {
+
+    class Pair {
+        TreeNode node;
+        int hd;
+
+        Pair(TreeNode node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public List<Integer> bottomView(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            TreeNode node = curr.node;
+            int hd = curr.hd;
+
+            map.put(hd, node.data);
+
+            if (node.left != null)
+                q.offer(new Pair(node.left, hd - 1));
+
+            if (node.right != null)
+                q.offer(new Pair(node.right, hd + 1));
+        }
+
+        for (int val : map.values())
+            ans.add(val);
+
+        return ans;
+    }
+}
