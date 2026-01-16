ee31df677e639eb29a7d3caa8ec1b9e24b145bef
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-01-16 19:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (86.92%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,29 +13,48 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+
+//         List<Integer> ans = new ArrayList<>();
+//         if (root == null) return ans;
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode curr = q.poll();
+
+//                 if (i == size - 1) {
+//                     ans.add(curr.val);
+//                 }
+
+//                 if (curr.left != null) q.offer(curr.left);
+//                 if (curr.right != null) q.offer(curr.right);
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-
         List<Integer> ans = new ArrayList<>();
-        if (root == null) return ans;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-
-                if (i == size - 1) {
-                    ans.add(curr.val);
-                }
-
-                if (curr.left != null) q.offer(curr.left);
-                if (curr.right != null) q.offer(curr.right);
-            }
-        }
+        dfs(root, 0, ans);
         return ans;
+    }
+    void dfs(TreeNode root, int d, List<Integer> ans) {
+        if (root == null) return;
+
+        if (d == ans.size()) ans.add(root.val);
+
+        dfs(root.right, d + 1, ans);
+
+        dfs(root.left, d + 1, ans);
     }
 }
