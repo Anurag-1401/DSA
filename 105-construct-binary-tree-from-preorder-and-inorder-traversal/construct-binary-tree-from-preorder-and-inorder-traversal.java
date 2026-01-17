d1e690bab9742da120954d68d2489cab443c675f
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-01-17 19:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (90.68%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,29 +13,50 @@
  *     }
  * }
  */
+
+// class Solution {
+//     int preI = 0;
+//     Map<Integer,Integer> map = new HashMap<>();
+
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         for(int i=0;i<inorder.length;i++){
+//             map.put(inorder[i],i);
+//         }
+
+//         return construct(preorder,0,inorder.length-1);
+//     }
+
+//     private TreeNode construct(int[] pre,int s,int e){
+//         if(s>e) return null;
+
+//         int rootVal = pre[preI++];
+//         TreeNode root = new TreeNode(rootVal);
+
+//         int idx = map.get(rootVal);
+
+//         root.left = construct(pre,s,idx-1);
+//         root.right = construct(pre,idx+1,e);
+
+//         return root;
+//     }
+// }
+
+
 class Solution {
-    int preI = 0;
-    Map<Integer,Integer> map = new HashMap<>();
-
+    private int i=0;
+    private int p=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        for(int i=0;i<inorder.length;i++){
-            map.put(inorder[i],i);
-        }
-
-        return construct(preorder,0,inorder.length-1);
+        return build(inorder,preorder,Integer.MIN_VALUE);
     }
-
-    private TreeNode construct(int[] pre,int s,int e){
-        if(s>e) return null;
-
-        int rootVal = pre[preI++];
-        TreeNode root = new TreeNode(rootVal);
-
-        int idx = map.get(rootVal);
-
-        root.left = construct(pre,s,idx-1);
-        root.right = construct(pre,idx+1,e);
-
-        return root;
+    public TreeNode build(int[] inorder,int[] preorder,int stop){
+        if(p>=preorder.length) return null;
+        if(inorder[i]==stop){
+            i++;
+            return null;
+        }
+        TreeNode node=new TreeNode(preorder[p++]);
+        node.left=build(inorder,preorder,node.val);
+        node.right=build(inorder,preorder,stop);
+        return node;
     }
 }
