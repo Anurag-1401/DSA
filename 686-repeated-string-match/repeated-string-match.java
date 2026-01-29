38c41b39bf67292c25be6b60838ee9342c50efc8
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-01-29 18:28:05 +0530

    Time: 20 ms (85.71%) | Memory: 47.4 MB (5.94%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,20 +1,67 @@
-class Solution {
-    public int repeatedStringMatch(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int  c = 0;
+// class Solution {
+//     public int repeatedStringMatch(String a, String b) {
+//         StringBuilder sb = new StringBuilder();
+//         int  c = 0;
         
-        while(sb.length()<b.length()){
-            sb.append(a);
-            c++;
+//         while(sb.length()<b.length()){
+//             sb.append(a);
+//             c++;
+//         }
+
+//         if(sb.indexOf(b) != -1) return c;
+
+//         sb.append(a);
+//         c++;
+
+//         if(sb.indexOf(b) != -1) return c;
+
+//         return -1;
+//     }
+// }
+
+class Solution {
+
+    boolean kmp(String text, String pat) {
+        String s = pat + "#" + text;
+        int[] lps = new int[s.length()];
+
+        for (int i = 1; i < s.length(); i++) {
+            int len = lps[i - 1];
+
+            while (len > 0 && s.charAt(i) != s.charAt(len)) {
+                len = lps[len - 1];
+            }
+
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+            }
+
+            lps[i] = len;
         }
 
-        if(sb.indexOf(b) != -1) return c;
+        int n = pat.length();
+        for (int i = n + 1; i < s.length(); i++) {
+            if (lps[i] == n)
+                return true;
+        }
 
-        sb.append(a);
-        c++;
+        return false;
+    }
 
-        if(sb.indexOf(b) != -1) return c;
+    public int repeatedStringMatch(String a, String b) {
+        int cnt = 1;
+        String t = a;
+        while(a.length()<b.length()){
+            a+=t;
+            cnt++;
+        }
+        if(a.equals(b)) return cnt;
 
-        return -1;
+        if(kmp(a,b)) return cnt;
+
+        cnt+=1;
+        if(kmp(a+t,b)) return cnt;
+        return -1; 
+
     }
 }
