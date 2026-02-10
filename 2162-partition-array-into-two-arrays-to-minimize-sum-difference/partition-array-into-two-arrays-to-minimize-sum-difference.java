5adf5ab44f3464e2d4c3774e61ab1c2fe8775018
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-10 16:43:06 +0530

    Time: 197 ms (98.96%) | Memory: 47.8 MB (98.51%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,71 +1,69 @@
 class Solution {
     public int minimumDifference(int[] nums) {
-        int n = nums.length / 2;
-        int totalSum = 0;
-        for (int x : nums) totalSum += x;
+        int n = nums.length;
+        int[][] diff1 = generate(Arrays.copyOfRange(nums, 0, n / 2));
+        int[][] diff2 = generate(Arrays.copyOfRange(nums, n / 2, n));
 
-        int[] left = Arrays.copyOfRange(nums, 0, n);
-        int[] right = Arrays.copyOfRange(nums, n, 2 * n);
+        int min = Integer.MAX_VALUE;
+        for (int len = 0; len <= n / 2; len++) {
+            int[] left = diff1[len];
+            int[] right = diff2[n/2 - len];
 
-        List<Integer>[] leftSums = new ArrayList[n + 1];
-        List<Integer>[] rightSums = new ArrayList[n + 1];
+            int l = 0;
+            int r = diff2[n/2 - len].length - 1;
 
-        for (int i = 0; i <= n; i++) {
-            leftSums[i] = new ArrayList<>();
-            rightSums[i] = new ArrayList<>();
-        }
-
-        generate(left, leftSums);
-        generate(right, rightSums);
-
-        for (int i = 0; i <= n; i++) {
-            Collections.sort(rightSums[i]);
-        }
-
-        int result = Integer.MAX_VALUE;
-
-        for (int k = 0; k <= n; k++) {
-            List<Integer> leftList = leftSums[k];
-            List<Integer> rightList = rightSums[n - k];
-
-            for (int x : leftList) {
-                int target = totalSum / 2 - x;
-
-                int idx = Collections.binarySearch(rightList, target);
-                if (idx < 0) idx = -idx - 1;
-
-                if (idx < rightList.size()) {
-                    int sum = x + rightList.get(idx);
-                    result = Math.min(result,
-                            Math.abs(totalSum - 2 * sum));
-                }
-
-                if (idx > 0) {
-                    int sum = x + rightList.get(idx - 1);
-                    result = Math.min(result,
-                            Math.abs(totalSum - 2 * sum));
-                }
+            while (l < left.length && r >= 0) {
+                int diff = left[l] + right[r];
+                min = Math.min(min, Math.abs(diff));
+                if (diff < 0)
+                    l++;
+                else if (diff > 0)
+                    r--;
+                else
+                    return 0;
             }
         }
 
-        return result;
+        return min;
     }
 
-    private void generate(int[] arr, List<Integer>[] sums) {
-        int n = arr.length;
-        int total = 1 << n;
+    private static int[][] generate(int[] nums) {
+        int n = nums.length;
+        int m = 1 << n;
 
-        for (int mask = 0; mask < total; mask++) {
-            int size = 0;
-            int sum = 0;
+        int total = 0;
+        for (int num : nums)
+            total += num;
 
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    size++;
-                    sum += arr[i];
-                }
-            }
-            sums[size].add(sum);
+        for (int i = 0; i < n; i++)
+            nums[i] <<= 1;
+
+        int[] sums = new int[m];
+        sums[0] -= total;
+
+        for (int i = 0, maxTo = 1; i < n; i++, maxTo <<= 1) {
+            int num = nums[i];
+            for (int from = 0, to = maxTo; from < maxTo; from++, to++)
+                sums[to] = sums[from] + num;
         }
+
+        int[][] ans = new int[n + 1][];
+        int[] pos = new int[n + 1];
+
+        for (int i = 0, binomial = 1; i <= n; i++) {
+            ans[i] = new int[binomial];
+            binomial = binomial * (n - i) / (i + 1);
+        }
+
+        for (int key = 0; key < m; key++) {
+            int bits = Integer.bitCount(key);
+            ans[bits][pos[bits]++] = sums[key];
+        }
+
+        for (int[] arr : ans)
+            Arrays.sort(arr);
+
+        return ans;
+        
     }
 }
