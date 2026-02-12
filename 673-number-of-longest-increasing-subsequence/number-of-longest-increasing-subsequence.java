a58f40a67bad15b97975ab859f799c37bac362b1
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-12 15:15:35 +0530

    Time: 7 ms (99.05%) | Memory: 46.5 MB (8.48%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,42 +1,35 @@
 class Solution {
+
+    private void collect(List<List<int[]>> seqs, int num, int idx){
+        if(seqs.size() == idx) seqs.add(new ArrayList<>());
+        int[] cur = new int[]{num, 0};
+        if(idx == 0) cur[1] = 1;
+        else{
+            for(int[] seq: seqs.get(idx-1)){
+                if(seq[0] < cur[0]) cur[1] += seq[1];
+            }
+        }
+        seqs.get(idx).add(cur);
+    }
+
     public int findNumberOfLIS(int[] nums) {
-
         int n = nums.length;
+        int[] dp = new int[n];
+        int length = 0;
+        List<List<int[]>> seqs = new ArrayList<>();
+        for(int num:nums){
+            int idx = Arrays.binarySearch(dp, 0, length, num);
+            if(idx<0) idx = -(idx + 1);
+            dp[idx] = num;
+            if(length == idx) length++;
 
-        int[] len = new int[n];
-        int[] count = new int[n];
-
-        int maxLen = 1;
-
-        for (int i = 0; i < n; i++) {
-            len[i] = 1;
-            count[i] = 1;
-
-            for (int prev = 0; prev < i; prev++) {
-
-                if (nums[i] > nums[prev]) {
-
-                    if (len[prev] + 1 > len[i]) {
-                        len[i] = len[prev] + 1;
-                        count[i] = count[prev];
-                    }
-                    else if (len[prev] + 1 == len[i]) {
-                        count[i] += count[prev];
-                    }
-                }
-            }
-
-            maxLen = Math.max(maxLen, len[i]);
+            //question specific
+            collect(seqs, num, idx);
         }
-
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (len[i] == maxLen) {
-                result += count[i];
-            }
+        int res = 0;
+        for(int[] seq: seqs.get(seqs.size()-1)){
+            res += seq[1];
         }
-
-        return result;
+        return res;
     }
 }
