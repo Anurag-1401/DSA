4df6500dc6033f6b792d40db037dea5b8a1c80d5
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-13 19:08:16 +0530

    Time: 30 ms (99.02%) | Memory: 44.5 MB (66.80%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -2,30 +2,27 @@ class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n + 2];
-        arr[0] = arr[n + 1] = 1;
-
+        arr[0] = 1;
+        arr[n + 1] = 1;
         System.arraycopy(nums, 0, arr, 1, n);
 
         int[][] dp = new int[n + 2][n + 2];
 
-        for (int len = 2; len < n + 2; len ++) {
-            int end = n + 2 - len;
-            for (int left = 0; left < end; left ++) {
-                int right = left + len;
-                int leftVal = arr[left];
-                int rightVal = arr[right];
-
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i <= n - len + 1; i++) {
+                int j = i + len - 1;
+                int common = arr[i - 1] * arr[j + 1];
                 int max = 0;
-
-                for (int k = left + 1; k < right; k ++) {
-                    int temp = leftVal * arr[k] * rightVal + dp[left][k] + dp[k][right];
-                    max = max > temp ? max : temp;
+                for (int k = i; k <= j; k++) {
+                    int current = common * arr[k] + dp[i][k - 1] + dp[k + 1][j];
+                    if (current > max) {
+                        max = current;
+                    }
                 }
-
-                dp[left][right] = max;
+                dp[i][j] = max;
             }
         }
 
-        return dp[0][n + 1];
+        return dp[1][n];
     }
 }
