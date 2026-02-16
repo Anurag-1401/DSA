2b50ef6b3140f0c6793bdfee4a6550017e973a5b
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-17 00:42:54 +0530

    Time: 1 ms (99.70%) | Memory: 44.2 MB (56.55%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,59 +1,41 @@
-import java.util.*;
-
 class Solution {
     public int orangesRotting(int[][] grid) {
+        if(grid==null || grid.length  ==0) return -1;
 
         int rows = grid.length;
         int cols = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
-        int fresh = 0;
+        int[][] time = new int[rows][cols];
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
+        for(int i=0;i<rows;i++)
+            Arrays.fill(time[i],Integer.MAX_VALUE);
+       
 
-                if (grid[r][c] == 2)
-                    q.offer(new int[]{r, c});
+        for(int i=0;i<rows;i++)
+            for(int j=0;j<cols;j++)
+            if(grid[i][j] == 2)
+                dfs(grid,time,i,j,0);
 
-                if (grid[r][c] == 1)
-                    fresh++;
-            }
-        }
+        int timerequired = 0;
+        for(int i=0;i<rows;i++)
+        for(int j=0;j<cols;j++)
+        if(grid[i][j] == 1){
+            if(time[i][j]==Integer.MAX_VALUE)return -1;
+            timerequired = Math.max(timerequired,time[i][j]);
+        }   
+        return timerequired;
+    }
 
-        if (fresh == 0) return 0;
+    private void dfs(int[][]grid,int[][] time,int i,int j,int currenttime){
 
-        int minutes = 0;
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]==0 ||
+             currenttime >= time[i][j]) return;
 
-        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
-        while (!q.isEmpty()) {
-
-            int size = q.size();
-            boolean rottedThisMinute = false;
-
-            for (int i = 0; i < size; i++) {
-
-                int[] cell = q.poll();
-                int r = cell[0], c = cell[1];
-
-                for (int[] d : dirs) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-
-                    if (nr >= 0 && nc >= 0 && nr < rows && nc < cols
-                            && grid[nr][nc] == 1) {
-
-                        grid[nr][nc] = 2;
-                        fresh--;
-                        q.offer(new int[]{nr, nc});
-                        rottedThisMinute = true;
-                    }
-                }
-            }
-
-            if (rottedThisMinute) minutes++;
-        }
-
-        return fresh == 0 ? minutes : -1;
+        time[i][j] =currenttime;
+        dfs(grid,time,i-1,j,currenttime+1);
+        dfs(grid,time,i+1,j,currenttime+1);
+        dfs(grid,time,i,j-1,currenttime+1);
+        dfs(grid,time,i,j+1,currenttime+1);
     }
 }
