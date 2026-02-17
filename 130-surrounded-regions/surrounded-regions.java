8e2c99a8c8e3b919282f12b59580d14a6f5525d2
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-17 14:12:20 +0530

    Time: 2 ms (81.46%) | Memory: 47.9 MB (56.46%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,44 +1,37 @@
 class Solution {
     public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
 
-        int rows = board.length;
-        int cols = board[0].length;
+        int m = board.length, n = board[0].length;
 
-        // Step 1: Traverse boundary
-        for (int r = 0; r < rows; r++) {
-            dfs(board, r, 0);
-            dfs(board, r, cols - 1);
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
         }
 
-        for (int c = 0; c < cols; c++) {
-            dfs(board, 0, c);
-            dfs(board, rows - 1, c);
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
         }
 
-        // Step 2: Flip surrounded & restore safe
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-
-                if (board[r][c] == 'O')
-                    board[r][c] = 'X';   // surrounded
-
-                if (board[r][c] == 'S')
-                    board[r][c] = 'O';   // safe restore
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == '#') board[i][j] = 'O';
             }
         }
     }
 
-    private void dfs(char[][] board, int r, int c) {
+    private void dfs(char[][] board, int i, int j) {
+        int m = board.length, n = board[0].length;
 
-        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length
-                || board[r][c] != 'O')
-            return;
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') return;
 
-        board[r][c] = 'S';  // mark safe
+        board[i][j] = '#';
 
-        dfs(board, r + 1, c);
-        dfs(board, r - 1, c);
-        dfs(board, r, c + 1);
-        dfs(board, r, c - 1);
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
     }
 }
