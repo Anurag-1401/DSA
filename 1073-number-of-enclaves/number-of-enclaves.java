4131baf8684fee4e3f15a52d08e6a57fa8e9f638
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-17 14:13:13 +0530

    Time: 5 ms (99.73%) | Memory: 60.9 MB (44.74%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,45 +1,31 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        // Step 1: Remove boundary-connected land
-        for (int r = 0; r < rows; r++) {
-            dfs(grid, r, 0);
-            dfs(grid, r, cols - 1);
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=0;i<m;i++){
+            dfs(grid,i,0);
+            dfs(grid,i,n-1);
         }
-
-        for (int c = 0; c < cols; c++) {
-            dfs(grid, 0, c);
-            dfs(grid, rows - 1, c);
+        for(int i=0;i<n;i++){
+            dfs(grid,0,i);
+            dfs(grid,m-1,i);
         }
-
-        // Step 2: Count remaining land
-        int count = 0;
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-
-                if (grid[r][c] == 1)
-                    count++;
+        int c=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                c+=grid[i][j];
             }
         }
-
-        return count;
+        return c;
     }
+    private void dfs(int g[][],int i,int j){
+        if(i<0 || j<0 || i>=g.length || j>=g[0].length || g[i][j]==0)
+            return ;
+        g[i][j]=0;
+        dfs(g,i+1,j);
+        dfs(g,i-1,j);
+        dfs(g,i,j+1);
+        dfs(g,i,j-1);
+    } 
 
-    private void dfs(int[][] grid, int r, int c) {
-
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length
-                || grid[r][c] == 0)
-            return;
-
-        grid[r][c] = 0;  // remove land
-
-        dfs(grid, r + 1, c);
-        dfs(grid, r - 1, c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
-    }
 }
