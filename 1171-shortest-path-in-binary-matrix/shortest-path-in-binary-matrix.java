a6539519a3f86f3d9f535849f706f3949ca124f3
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-18 17:51:16 +0530

    Time: 9 ms (99.83%) | Memory: 48.2 MB (10.68%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,47 +1,48 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-
+        int[] dir = {1, 1, -1, -1, 1, 0, -1, 0, 1};
         int n = grid.length;
+        int dl = dir.length;
 
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
+        int[] q = new int[n * n];
+        int head = 0, tail = 0;
+
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] == 1)
             return -1;
 
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][n];
+        q[tail++] = 0;
+        grid[0][0] = 1;
 
-        q.offer(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        int c = 1;
+        boolean isFounded = false;
+        outer_loop:
+        while (head != tail) {
+            int size = tail - head;
 
-        int[][] dir = {
-            {1,0}, {-1,0}, {0,1}, {0,-1},
-            {1,1}, {1,-1}, {-1,1}, {-1,-1}
-        };
+            for (int ign = 0; ign < size; ign++) {
+                int val = q[head++];
+                int i = val / n, j = val - i * n;
 
-        while (!q.isEmpty()) {
+                if (i == n - 1 && j == n - 1) {
+                    isFounded = true;
+                    break outer_loop;
+                }
 
-            int[] curr = q.poll();
-            int r = curr[0];
-            int c = curr[1];
-            int dist = curr[2];
+                for (int d = 0; d < dl - 1; d++) {
+                    int ni = i + dir[d], nj = j + dir[d + 1];
 
-            if (r == n-1 && c == n-1)
-                return dist;
+                    if (ni < 0 || ni >= n || nj < 0 || nj >= n || grid[ni][nj] == 1)
+                        continue;
 
-            for (int[] d : dir) {
-
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if (nr >= 0 && nc >= 0 && nr < n && nc < n
-                        && grid[nr][nc] == 0
-                        && !visited[nr][nc]) {
-
-                    visited[nr][nc] = true;
-                    q.offer(new int[]{nr, nc, dist + 1});
+                    q[tail++] = ni * n + nj;
+                    grid[ni][nj] = 1;
                 }
             }
+
+            c++;
         }
 
-        return -1;
+        return isFounded ? c : -1;
+
     }
 }
