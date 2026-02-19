c251780841ddeaf6740629da01d9821f5c2f5fb8
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-02-19 16:22:08 +0530

    Time: 2 ms (99.83%) | Memory: 46.8 MB (74.93%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,66 +1,30 @@
-import java.util.*;
-
 class Solution {
+    private int connectedComponentCount = 0;
 
-    int[] parent;
-    int[] size;
-
-    public int removeStones(int[][] stones) {
-
-        int OFFSET = 10001;  // Safe shift for columns
-        int MAX = 20005;
-
-        parent = new int[MAX];
-        size = new int[MAX];
-
-        for (int i = 0; i < MAX; i++) {
-            parent[i] = i;
-            size[i] = 1;
+    public int removeStones(int[][] stonePositions) {
+        int[] setRepresentatives = new int[20003];
+        for (int[] stonePosition : stonePositions) {
+            mergeComponents(stonePosition[0] + 1, stonePosition[1] + 10002, setRepresentatives);
         }
-
-        Set<Integer> usedNodes = new HashSet<>();
-
-        // Step 1: Union row ↔ column
-        for (int[] stone : stones) {
-
-            int row = stone[0];
-            int col = stone[1] + OFFSET;
-
-            union(row, col);
-
-            usedNodes.add(row);
-            usedNodes.add(col);
-        }
-
-        // Step 2: Count components
-        int components = 0;
-
-        for (int node : usedNodes) {
-            if (find(node) == node)
-                components++;
-        }
-
-        return stones.length - components;
+        return stonePositions.length - connectedComponentCount;
     }
 
-    private int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
+    private int findRepresentative(int element, int[] setRepresentatives) {
+        if (setRepresentatives[element] == 0) {
+            setRepresentatives[element] = element;
+            connectedComponentCount++;
+        }
+        return setRepresentatives[element] == element ? element : 
+               (setRepresentatives[element] = findRepresentative(setRepresentatives[element], setRepresentatives));
     }
 
-    private void union(int a, int b) {
-
-        int pa = find(a);
-        int pb = find(b);
-
-        if (pa == pb) return;
-
-        if (size[pa] < size[pb]) {
-            parent[pa] = pb;
-            size[pb] += size[pa];
-        } else {
-            parent[pb] = pa;
-            size[pa] += size[pb];
+    private void mergeComponents(int elementA, int elementB, int[] setRepresentatives) {
+        int repA = findRepresentative(elementA, setRepresentatives);
+        int repB = findRepresentative(elementB, setRepresentatives);
+        if (repA != repB) {
+            setRepresentatives[repB] = repA;
+            connectedComponentCount--;
         }
     }
 }
+
