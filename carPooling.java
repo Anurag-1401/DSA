ef03b3d22bf2f799d7088a01205c0dc10b16565d
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-03-27 17:53:33 +0530

    Create carPooling.java

---
+++ carPooling.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] changes = new int[1001];
+
+    for (int[] t : trips) {
+        changes[t[1]] += t[0];
+        changes[t[2]] -= t[0];
+    }
+
+    int current = 0;
+
+    for (int i = 0; i < 1001; i++) {
+        current += changes[i];
+
+        if (current > capacity) return false;
+    }
+
+    return true;
+    }
+}
