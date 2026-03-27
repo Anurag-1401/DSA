db816dc2873471967ad59edb3a05124ed573e19d
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-03-27 17:58:35 +0530

    Create findMedianFromDS.java

---
+++ findMedianFromDS.java
@@ -0,0 +1,30 @@
+class MedianFinder {
+
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>((a, b) -> a - b);
+    }
+
+    public void addNum(int num) {
+
+        maxHeap.add(num);
+
+        minHeap.add(maxHeap.poll());
+
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return maxHeap.peek();
+        }  
+    }
+}
