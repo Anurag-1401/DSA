e6b2bee93b32cfe4651dd98095ff35480d3d0e4e
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-04-17 10:19:50 +0530

    Update unique-paths.java

--- Strivers Complete/62-unique-paths/unique-paths.java
+++ Strivers Complete/62-unique-paths/unique-paths.java
@@ -6,7 +6,7 @@ class Solution {
 
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                dp[j] = dp[j] + dp[j-1];
+                dp[j] += dp[j-1];
             }
         }
 
