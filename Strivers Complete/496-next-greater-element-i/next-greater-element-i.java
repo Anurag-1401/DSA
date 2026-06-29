064565f6331feb47ef8f64424ab6faf14bbfbcd9
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-06-29 15:08:04 +0530

    Update next-greater-element-i.java

--- Strivers Complete/496-next-greater-element-i/next-greater-element-i.java
+++ Strivers Complete/496-next-greater-element-i/next-greater-element-i.java
@@ -26,3 +26,31 @@ class Solution {
         return -1;
     }
 }
+
+
+
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         Map<Integer,Integer> map = new HashMap<>();
+//         int n1 = nums1.length;
+//         int n2 = nums2.length;
+        
+//         int[] st  = new  int[n2];
+//         int top = -1;
+    
+//         for(int n:nums2){
+//             while(top>=0 && n>st[top]) map.put(st[top--],n);
+//             st[++top] = n;
+//         }
+
+//         while(top>=0) map.put(st[top--],-1);
+
+//         int[] ans = new int[n1];
+
+//         for(int i=0;i<n1;i++){
+//             ans[i] = map.get(nums1[i]);
+//         }
+
+//         return ans;
+//     }
+// }
