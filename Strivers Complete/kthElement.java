5184e18190dabd5287a6349a01ba1654963da3de
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-07-15 22:56:30 +0530

    Update kthElement.java

--- Strivers Complete/kthElement.java
+++ Strivers Complete/kthElement.java
@@ -1,27 +1,28 @@
 class Solution {
     public int kthElement(int[] a, int[] b, int k) {
-      int m = a.length,n = b.length;
+      int m = a.length, n = b.length;
 
-      if(m > n) return kthElement(b,a,k);
+      if(m > n) return kthElement(b, a, k);
 
-      
-      int low = Math.max(0,k-n),high = Math.min(k,m);
+      int low = Math.max(0, k - n), high = Math.min(k, m);
 
-      while(low<=high){
+      while(low <= high){
         int parX = (low + high) / 2;
-        int parY  = k - parX;
+        int parY = k - parX;
 
         int maxLeftX = (parX == 0) ? Integer.MIN_VALUE : a[parX - 1];
         int minRightX = (parX == m) ? Integer.MAX_VALUE : a[parX];
 
         int maxLeftY = (parY == 0) ? Integer.MIN_VALUE : b[parY - 1];
-        int minRightY = (parY == n) ? Integer.MAX_VALUE : a[parY];
+        int minRightY = (parY == n) ? Integer.MAX_VALUE : b[parY];
 
         if(maxLeftX <= minRightY && maxLeftY <= minRightX){
-            return Math.max(maxLeftX,maxLeftY);
-        } else if(maxLeftX > maxRightY) high = parX - 1;
-        else low = parX + 1;
-
+            return Math.max(maxLeftX, maxLeftY);
+        } else if(maxLeftX > minRightY) {
+            high = parX - 1;
+        } else {
+            low = parX + 1;
+        }
       }
       return -1;
     }
