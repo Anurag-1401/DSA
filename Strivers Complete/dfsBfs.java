6322a3ee0e307a19da6e9f0f5c98b33175acffbd
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-07-27 15:33:56 +0530

    Update dfsBfs.java

--- Strivers Complete/dfsBfs.java
+++ Strivers Complete/dfsBfs.java
@@ -1,46 +1,64 @@
-class Solution {
-   public List<Integer> dfsOfGraph(int V, List<List<Integer>> adj) {
-        boolean[] visited = new boolean[V];
+class Solution { 
+    public List<Integer> dfsOfGraph(int V, List<List<Integer>> edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (List<Integer> edge : edges) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
         List<Integer> result = new ArrayList<>();
+        boolean[] visited = new boolean[V];
 
-        dfs(0, adj, visited, result);
-
+        dfsHelper(0, adj, visited, result);
         return result;
     }
 
-    private void dfs(int node, List<List<Integer>> adj, boolean[] visited, List<Integer> result) {
+    private void dfsHelper(int node, List<List<Integer>> adj, boolean[] visited, List<Integer> result) {
         visited[node] = true;
         result.add(node);
 
-        for (int neigh : adj.get(node)) {
-            if (!visited[neigh]) {
-                dfs(neigh, adj, visited, result);
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfsHelper(neighbor, adj, visited, result);
             }
         }
     }
-    
-        public List<Integer> bfsOfGraph(int V, List<List<Integer>> adj) {
-        boolean[] visited = new boolean[V];
+
+    public List<Integer> bfsOfGraph(int V, List<List<Integer>> edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (List<Integer> edge : edges) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
         List<Integer> result = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
 
-        Queue<Integer> q = new LinkedList<>();
-
+        queue.add(0);
         visited[0] = true;
-        q.offer(0);
 
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            result.add(node);
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            result.add(curr);
 
-            for (int neigh : adj.get(node)) {
-                if (!visited[neigh]) {
-                    visited[neigh] = true;
-                    q.offer(neigh);
+            for (int neighbor : adj.get(curr)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
                 }
             }
         }
-
         return result;
-    }
+    } 
 }
-
