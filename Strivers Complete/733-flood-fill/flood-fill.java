059336992535cde6ec26bd86a4711279e76ddd68
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-07-27 15:43:18 +0530

    Update flood-fill.java

--- Strivers Complete/733-flood-fill/flood-fill.java
+++ Strivers Complete/733-flood-fill/flood-fill.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-
+ 
         int original = image[sr][sc];
 
         if (original == color) return image;
