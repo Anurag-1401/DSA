c58c448b576676889e4d7f4b76cb7c108c65fe6a
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-08-03 22:03:45 +0530

    Update two-sum.py

--- Leet-Code 75 II/1-two-sum/two-sum.py
+++ Leet-Code 75 II/1-two-sum/two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         num = 0
-
+ 
         for i in range(len(nums)):
             num = target - nums[i]
             if num in map: return [i,map[num]]
