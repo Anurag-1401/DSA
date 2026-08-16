2d551aca94c1dc35cff1601720c7f3ae5a4b3557
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-08-16 14:54:55 +0530

    Update longest-consecutive-sequence.java

--- Strivers Some/128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ Strivers Some/128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -6,7 +6,7 @@ class Solution {
         Arrays.sort(nums);
         int res = 0, curr = nums[0], streak = 0, i = 0;
 
-        while (i < nums.length) {
+        while (i < nums.length) { 
             if (curr != nums[i]) {
                 curr = nums[i];
                 streak = 0;
