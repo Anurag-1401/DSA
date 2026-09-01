ae0fb2f88c22857307ef31f8f93806956d07274e
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-01 15:36:20 +0530

    Update two-sum.java

--- Strivers Some/1-two-sum/two-sum.java
+++ Strivers Some/1-two-sum/two-sum.java
@@ -7,7 +7,7 @@ class Solution {
             num = target - nums[i];
             if(map.containsKey(num)) return new int[]{map.get(num),i};
             map.put(nums[i],i);
-        }
+        } 
         return new int[]{-1,-1};
     }
 }
