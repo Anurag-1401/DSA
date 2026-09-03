19fa3aaf46160d136f699cf61de29f19a0b8eee4
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-03 14:17:14 +0530

    Time: 2 ms (99.84%) | Memory: 46.4 MB (88.37%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,24 +1,50 @@
+// class Solution {
+//     public String convert(String s, int numRows) {
+//         if(numRows <= 1 || s.length() <= numRows) return s;
+
+//         StringBuilder[] rows = new StringBuilder[numRows];
+//         for(int i = 0;i<numRows;i++) rows[i] = new StringBuilder();
+
+//         int curr = 0;
+//         boolean down = false;
+
+//         for(int i = 0;i<s.length();i++){
+//             rows[curr].append(s.charAt(i));
+
+//             if(curr == 0 || curr == numRows-1) down = !down;
+
+//             curr = down ? curr+1 : curr-1; 
+//         }
+
+//         String ans = "";
+//         for(StringBuilder row:rows) ans+=row;
+
+//         return ans;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public String convert(String s, int numRows) {
-        if(numRows <= 1 || s.length() <= numRows) return s;
+        if(numRows == 1 || s.length() <= numRows) return s;
+        
+        StringBuilder sb = new StringBuilder();
 
-        StringBuilder[] rows = new StringBuilder[numRows];
-        for(int i = 0;i<numRows;i++) rows[i] = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
 
-        int curr = 0;
-        boolean down = false;
-
-        for(int i = 0;i<s.length();i++){
-            rows[curr].append(s.charAt(i));
-
-            if(curr == 0 || curr == numRows-1) down = !down;
-
-            curr = down ? curr+1 : curr-1; 
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0 ; j + i < n; j += cycleLen){
+                sb.append(s.charAt(j + i));
+                if(i != 0 && i != numRows - 1 && j + cycleLen - i < n){
+                    sb.append(s.charAt(j + cycleLen - i));
+                }
+            }
         }
-
-        String ans = "";
-        for(StringBuilder row:rows) ans+=row;
-
-        return ans;
+        
+        return sb.toString();
     }
 }
