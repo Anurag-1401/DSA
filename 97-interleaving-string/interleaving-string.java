bf33941b89aef544fa40d87cfb544653160cdaec
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-10 16:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (24.39%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int n = s1.length(), m = s2.length();
-        if (n + m != s3.length()) return false;
-        
-        boolean[][] dp = new boolean[n + 1][m + 1];
-        dp[0][0] = true;
-        
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i > 0 && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
-                    dp[i][j] = dp[i][j] || dp[i - 1][j];
-                }
-                if (j > 0 && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
-                    dp[i][j] = dp[i][j] || dp[i][j - 1];
-                }
-            }
+        int n = s1.length();
+        int m = s2.length();
+        int s = s3.length();
+        if(n+m!=s) return false;
+        Boolean[][] dp = new Boolean[n+1][m+1];
+        return memo(n,m,s1,s2,s3,dp);
+    }
+    public boolean memo(int i,int j,String s1,String s2,String s3,Boolean[][] dp){
+        if(i==0 && j==0) return true;
+        if(dp[i][j]!=null) return dp[i][j];
+        boolean bool = false;
+        int k = i+j;
+        if(i>0 && s3.charAt(k-1)==s1.charAt(i-1)){
+            bool = bool|memo(i-1,j,s1,s2,s3,dp);
         }
-        
-        return dp[n][m];
+        if(bool) return dp[i][j] = bool;
+        if(j>0 && s3.charAt(k-1)==s2.charAt(j-1)){
+            bool = bool|memo(i,j-1,s1,s2,s3,dp);
+        }
+        return dp[i][j] = bool;
     }
 }
