87f3d6dfc20d6e52343e8dd16fd710ec7f9d4345
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-14 13:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (22.04%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -8,40 +8,65 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public void reorderList(ListNode head) {
+//         if (head == null || head.next == null) return;
+
+//         ListNode slow = head, fast = head;
+//         while (fast.next != null && fast.next.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+
+//         ListNode second = reverse(slow.next);
+//         slow.next = null;
+
+//         ListNode first = head;
+//         while (second != null) {
+//             ListNode temp1 = first.next;
+//             ListNode temp2 = second.next;
+
+//             first.next = second;
+//             second.next = temp1;
+
+//             first = temp1;
+//             second = temp2;
+//         }
+//     }
+
+//     private ListNode reverse(ListNode head) {
+//         ListNode prev = null, curr = head;
+//         while (curr != null) {
+//             ListNode next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//         }
+//         return prev;
+//     }
+// }
+
+
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) return;
-
-        ListNode slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        ListNode second = reverse(slow.next);
-        slow.next = null;
-
-        ListNode first = head;
-        while (second != null) {
-            ListNode temp1 = first.next;
-            ListNode temp2 = second.next;
-
-            first.next = second;
-            second.next = temp1;
-
-            first = temp1;
-            second = temp2;
-        }
+        recurse(head, head);
     }
 
-    private ListNode reverse(ListNode head) {
-        ListNode prev = null, curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+    private ListNode recurse(ListNode head, ListNode fast){
+        if(fast.next == null){
+            ListNode tail = head.next;
+            head.next = null;
+            return tail;
+        } else if(fast.next.next == null){
+            ListNode tail = head.next.next;
+            head.next.next = null;
+            return tail;
         }
-        return prev;
+
+        ListNode tail = recurse(head.next, fast.next.next);
+        ListNode nextTail = tail.next;
+        tail.next = head.next;
+        head.next = tail;
+        return nextTail;
     }
 }
