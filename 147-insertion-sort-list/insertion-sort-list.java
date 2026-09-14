813dafde4991ea0df268b1bbba62a7cfa16a8161
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-14 13:38:46 +0530

    Time: 3 ms (96.56%) | Memory: 46.5 MB (33.77%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -13,24 +13,27 @@ class Solution {
         if (head == null || head.next == null) {
             return head;
         }
-
-        ListNode dummy = new ListNode(0);
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode nextTemp = curr.next;
-
-            ListNode prev = dummy;
-            while (prev.next != null && prev.next.val < curr.val) {
+        
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        ListNode prev = dummy; 
+        
+        while (head != null) {
+            ListNode next = head.next;
+            
+            if (prev.val >= head.val) {
+                prev = dummy; 
+            }
+            
+            while (prev.next != null && prev.next.val < head.val) {
                 prev = prev.next;
             }
-
-            curr.next = prev.next;
-            prev.next = curr;
-
-            curr = nextTemp;
+            
+            head.next = prev.next;
+            prev.next = head;
+            
+            head = next;
         }
-
+        
         return dummy.next;
     }
-}
+}   
