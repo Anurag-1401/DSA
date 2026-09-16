89b72efde08171e8f6e3a00954514400e4edd4d7
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-16 14:50:03 +0530

    Time: 482 ms (26.48%) | Memory: 273.4 MB (29.08%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,15 +1,24 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen = new HashSet<>();
-        Set<String> repeated = new HashSet<>();
-
-        for (int i = 0; i <= s.length() - 10; i++) {
-            String sub = s.substring(i, i + 10);
-            if (!seen.add(sub)) {
-                repeated.add(sub);
-            }
+        if (s.length() < 10) {
+            return new ArrayList<>();
         }
 
-        return new ArrayList<>(repeated);
+        Set<String> occurance = new HashSet<>();
+        Set<String> seen = new HashSet<>();
+
+        for(int i = 0; i <= s.length() - 10; i++) {
+            String str = s.substring(i, i+10);
+            
+            if (occurance.contains(str)) {
+                seen.add(str);
+            } 
+            
+            occurance.add(str);
+
+        }
+
+        return new ArrayList<>(seen);
+        
     }
 }
