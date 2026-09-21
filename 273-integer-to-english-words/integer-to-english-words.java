6a8e55596a7d714c9e3da225eec8412260a2cf2f
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-21 14:22:06 +0530

    Time: 1 ms (100.00%) | Memory: 43.2 MB (98.42%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,36 +1,84 @@
 class Solution {
-    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
-    private final String[] TENS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    
-    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+    String[] ones = {
+        "", "One", "Two", "Three", "Four",
+        "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen",
+        "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    String[] tens = {
+        "", "", "Twenty", "Thirty", "Forty",
+        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
 
     public String numberToWords(int num) {
-        if (num == 0) return "Zero";
 
-        int i = 0;
-        String words = "";
-
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
-            }
-            num /= 1000;
-            i++;
+        if (num == 0) {
+            return "Zero";
         }
 
-        return words.trim();
+        return helper(num);
     }
 
     private String helper(int num) {
-        if (num == 0) {
-            return "";
-        } else if (num < 20) {
-            return LESS_THAN_20[num] + " ";
-        } else if (num < 100) {
-            return TENS[num / 10] + " " + helper(num % 10);
-        } else {
-            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+
+        StringBuilder sb = new StringBuilder();
+
+        if (num >= 1000000000) {
+            sb.append(helper(num / 1000000000));
+            sb.append(" Billion");
+            num %= 1000000000;
+
+            if (num > 0) {
+                sb.append(" ");
+            }
         }
+
+        if (num >= 1000000) {
+            sb.append(helper(num / 1000000));
+            sb.append(" Million");
+            num %= 1000000;
+
+            if (num > 0) {
+                sb.append(" ");
+            }
+        }
+
+        if (num >= 1000) {
+            sb.append(helper(num / 1000));
+            sb.append(" Thousand");
+            num %= 1000;
+
+            if (num > 0) {
+                sb.append(" ");
+            }
+        }
+
+        if (num >= 100) {
+            sb.append(ones[num / 100]);
+            sb.append(" Hundred");
+            num %= 100;
+
+            if (num > 0) {
+                sb.append(" ");
+            }
+        }
+
+        if (num >= 20) {
+            sb.append(tens[num / 10]);
+            num %= 10;
+
+            if (num > 0) {
+                sb.append(" ");
+            }
+        }
+
+        if (num > 0) {
+            sb.append(ones[num]);
+        }
+
+        return sb.toString();
     }
 }
