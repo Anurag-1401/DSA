a7879a74b6f5252c734e2022ef8beb1c402595fd
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2026-09-21 14:30:16 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (46.37%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int papersCount = n - mid;
+            
+            if (citations[mid] >= papersCount) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return n - left;
+    }
+}
